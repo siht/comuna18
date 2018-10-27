512eff5044a2521a318aa836312b7391af3296fc
carlos <radix_10339@hotmail.com>
2018-10-26 20:15:17 -0500

    Register models into admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    Contact,
+    ContactPhone,
+    ContactEmail,
+)
 
-# Register your models here.
+admin.site.register(Contact)
+admin.site.register(ContactPhone)
+admin.site.register(ContactEmail)
