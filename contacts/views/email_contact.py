388820bd01a4df57747c822192293252a83ca079
carlos <radix_10339@hotmail.com>
2018-10-28 09:38:39 -0600

    Add libraries for other views

--- contacts/views/email_contact.py
+++ contacts/views/email_contact.py
@@ -1,4 +1,8 @@
-from django.views.generic import detail
+from django.views.generic import (
+    detail,
+    edit,
+    list as _list,
+)
 from django.urls import reverse_lazy
 from ..models import ContactEmail
 
